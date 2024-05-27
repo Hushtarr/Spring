c0cab7287ab7b779061153d81fa5d17bc4b7c895
hushtar <HUXTAR.EZ@gmail.com>
2024-05-27 11:51:18 +0200

    Update

---
+++ Spring_Data_I/src/main/java/org/spring/spring_data_i/SpringDataIApplication.java
@@ -0,0 +1,13 @@
+package org.spring.spring_data_i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataIApplication.class, args);
+	}
+
+}
---
+++ Spring_Data_I/src/test/java/org/spring/spring_data_i/SpringDataIApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.spring_data_i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
