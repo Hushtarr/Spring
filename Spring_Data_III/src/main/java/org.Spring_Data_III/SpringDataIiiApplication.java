cbfce2aa4491a969d702b959f558c9db66d41d28
hushtar <HUXTAR.EZ@gmail.com>
2024-05-29 10:35:23 +0200

    Update

---
+++ Spring_Data_III/src/main/java/org.Spring_Data_III/SpringDataIiiApplication.java
@@ -0,0 +1,13 @@
+package org.Spring_Data_III;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataIiiApplication.class, args);
+	}
+
+}
---
+++ Spring_Data_III/src/main/java/org.Spring_Data_III/bootstrap/Generator.java
@@ -0,0 +1,12 @@
+package org.Spring_Data_III.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Generator implements CommandLineRunner
+    {
+    @Override
+    public void run(String... args) throws Exception
+        {
+
+        }
+    }
